a5ca107290f80be0725493f167ac31a5345d182f
XIAOZHUXUEJAVA <2393751426@qq.com>
2022-11-12 13:40:39 +0800

    init

---
+++ data/authors/default.md
@@ -0,0 +1,16 @@
+---
+name: Xiao Zhu
+avatar: /static/images/avatar.jpg
+occupation: Student And Study in SDJZU
+company: This is the Future
+email: xiaozhuzhulzq@163.com
+twitter: https://twitter.com/Twitter
+linkedin: https://www.linkedin.com
+github: https://github.com/XIAOZHUXUEJAVA
+---
+
+这是过去：喜欢读书
+
+这是现在：什么也不喜欢
+
+这是未来：遨游
---
+++ data/projectsData.js
@@ -0,0 +1,18 @@
+const projectsData = [
+  {
+    title: 'CSDN在哪里?',
+    description: `来中文社区看我的博客！`,
+    imgSrc: '/static/images/csdn.png',
+    href: 'https://blog.csdn.net/qq_54650406?type=blog',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
